05e5aff6e41e3047aa2d5e94ac028e5d5584223f
KaterinaRomanova <44569848+KaterinaRomanova@users.noreply.github.com>
2020-02-26 11:15:16 +0300

    feat: add function min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array == null || array == undefined || array.length == 0){
+    return 0;
+  }
+  var min = array[0];
+  for(var i = 0; i < array.length; i++){
+    if(min > array[i]){
+      min = array[i];
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array == null || array == undefined || array.length == 0){
+    return 0;
+  }
+  var max = array[0];
+  for(var i = 0; i < array.length; i++){
+    if(max < array[i]){
+      max = array[i];
+    }
+  }
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array){
+  if(array == null || array == undefined || array.length == 0){
+    return 0;
+  }
+  var sum = 0;
+  for(var i = 0; i < array.length; i++){
+      sum = sum + array[i];
+    }
+var avg = sum/array.length;
+  return avg;
 }
